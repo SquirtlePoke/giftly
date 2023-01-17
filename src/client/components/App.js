47880c2610afb3326c30f151b8359bd323fd265f
JH51 <jonhuang5@gmail.com>
2023-01-17 15:52:37 -0500

    Restructure folders

---
+++ src/client/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <h1>App</h1>
+    </BrowserRouter>
+  );
+};
+
+export default App;
