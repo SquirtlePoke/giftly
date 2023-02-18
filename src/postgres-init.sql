ee56c9cba6c28083d06218feac36fb1d32a93854
JH51 <jonhuang5@gmail.com>
2023-02-18 01:48:48 -0500

    Add: sessions table

--- src/postgres-init.sql
+++ src/postgres-init.sql
@@ -6,28 +6,27 @@ CREATE TABLE users(
    password VARCHAR(200) NOT NULL,
    PRIMARY KEY(user_id)
 );
-
+CREATE TABLE sessions(
+   session_id VARCHAR NOT NULL,
+   date_created TIMESTAMPTZ NOT NULL,
+   user_id INT NOT NULL,
+   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
 CREATE TABLE collections(
    collection_id INT GENERATED ALWAYS AS IDENTITY,
    name VARCHAR(50) NOT NULL,
    user_id INT,
    PRIMARY KEY(collection_id),
-   CONSTRAINT fk_user
-      FOREIGN KEY(user_id)
-	  REFERENCES users(user_id)
+   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
-
-
 CREATE TABLE items(
    item_id INT GENERATED ALWAYS AS IDENTITY,
    name VARCHAR(50) NOT NULL,
-   price INT,
+   price NUMERIC,
    link VARCHAR NOT NULL,
    description VARCHAR(250) NOT NULL,
    image_link VARCHAR NOT NULL,
    collection_id INT,
    PRIMARY KEY(item_id),
-   CONSTRAINT fk_collection
-      FOREIGN KEY(collection_id)
-	  REFERENCES collections(collection_id)
+   CONSTRAINT fk_collection FOREIGN KEY(collection_id) REFERENCES collections(collection_id)
 );
